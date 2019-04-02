ddb95706fa24d7aee4440dad5e2cefc03acc6589
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-04-02 11:59:44 +0530

    initial commit: introduce entities

---
+++ src/Board.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Board extends React.Component {
+  renderSquare(id) {
+    return <Square />;
+  }
+
+  render() {
+    const status = "Next Player:X";
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
