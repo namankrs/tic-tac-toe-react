90152462ca9397e5e2d1c23a6d78f983ac491304
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-04-02 23:14:48 +0530

    added all features : CSS has to be added

--- src/Board.js
+++ src/Board.js
@@ -3,59 +3,18 @@ import Square from "./Square";
 import "./index.css";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { squares: Array(9).fill(null), xIsNext: true };
-  }
-
-  handleClick(id) {
-    const squares = this.state.squares.slice();
-    if (this.hasWon(squares) || !(squares[id] == null)) return;
-    squares[id] = this.state.xIsNext ? "X" : "O";
-    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
-  }
-
   renderSquare(id) {
     return (
       <Square
-        value={this.state.squares[id]}
-        onClick={() => this.handleClick(id)}
+        value={this.props.squares[id]}
+        onClick={() => this.props.onClick(id)}
       />
     );
   }
 
-  hasWon(squares) {
-    const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-
-    for (let i = 0; i < winningCombinations.length; i++) {
-      const [a, b, c] = winningCombinations[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
   render() {
-    let status = `Next Player:${this.state.xIsNext ? "X" : "O"}`;
-    const winner = this.hasWon(this.state.squares);
-    if (winner) status = `${winner} has won the game`;
     return (
       <div className="rows">
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
